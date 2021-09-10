ffeeae18ea0c7e27c43a927e7d88a52a4c8cfe87
Gareth Williams <6419944+Gaweph@users.noreply.github.com>
2021-09-10 13:34:56 +0100

    Implemented FIxedITemPromotion

--- PromotionEngine/Promotions/FixedPricePromotion.cs
+++ PromotionEngine/Promotions/FixedPricePromotion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PromotionEngine.Promotions
 {
@@ -14,9 +15,48 @@ namespace PromotionEngine.Promotions
             Price = price;
         }
 
-        public IEnumerable<PromotionMatch> GetMatches(IEnumerable<CartItem> allItems)
+        public IEnumerable<PromotionMatch> GetMatches(IEnumerable<CartItem> items)
         {
-            throw new NotImplementedException();
+            // Only interested in subset of items
+            var itemsOfInterest = items.Where(x => Ids.Contains(x.Id)).ToList();
+
+            var matches = new List<PromotionMatch>();
+            while (TryGetNextMatch(itemsOfInterest, out var match))
+            {
+                matches.Add(match);
+            }
+
+            return matches;
+        }
+
+        private bool TryGetNextMatch(List<CartItem> items, out PromotionMatch match)
+        {
+            var matchingItems = new List<CartItem>();
+            foreach (var id in Ids)
+            {
+                var item = items.FirstOrDefault(x => x.Id == id);
+                if (item != null)
+                {
+                    matchingItems.Add(item);
+                    items.Remove(item);
+                }
+            }
+
+            if (matchingItems.Count == Ids.Length)
+            {
+                match = new PromotionMatch
+                {
+                    GroupPrice = Price,
+                    Items = matchingItems
+                };
+                return true;
+            }
+            else
+            {
+                match = null;
+                return false;
+            }
+
         }
     }
 
